f8cf8c1ab642d3a60872a0048154b931c00ee64f
gauravpund <pundgaurav81@gmail.com>
2024-02-24 10:29:05 +0530

    first commit

---
+++ src/test/java/com/InfiniteStore/InfiniteStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.InfiniteStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfiniteStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
